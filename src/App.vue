003f085a054e75c6cc1e77324f48bbdbbfa4aacb
toru <t02f447f@gmail.com>
2021-02-20 07:22:14 +0900

    IMPROVE APP.vue HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <HellloWorld />
+  <HelloWorld :msg=100 :user="user" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld, { User } from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      person:{
+        firstName: 'john',
+        lastName: 'doe',
+        age: 32,
+      }
+    }
+  },
+  computed: {
+    user(): User {
+      return this.person;
+    }
   }
 });
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,61 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ book.title }}</h1>
+    <h2>{{ book.author }}</h2>
+    <h2>{{ book.year }}</h2>
+    <h3>{{ msg }}</h3>
+
+    <h2>{{ fullName }} {{ this.user.age }}</h2>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, PropType } from 'vue';
+
+interface Book{
+  title: string;
+  author: string;
+  year: number;
+}
+
+export interface User {
+  firstName: string;
+  lastName: string;
+  age: number;
+}
 
 export default defineComponent({
   name: 'HelloWorld',
   data() {
     return {
-      msg: 'Hello Typescript'
+      book: {
+        title: 'Vue 3 Guide',
+        author: 'Vue Team',
+        year: 2021,
+      } as Book
     }
   },
+  props:{
+    msg:{
+      type: Number as PropType<number>
+    },
+    user: {
+      type: Object as PropType<User>,
+      required: true,
+    }
+  },
+  computed: {
+  getLength(): number{
+    return this.book.title.length
+  },
+  fullName(): string {
+    return this.user.firstName + ' ' + this.user.lastName;
+  },
+  // methods:{
+  //   changeMsg(){
+  //     this.book.year = 2022;
+  //   }
+  // },
+  }
 });
 </script>
